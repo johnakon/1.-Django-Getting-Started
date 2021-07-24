49be935bfc90bd62c4447166c2c714082fb43918
johnakon <john.akonya@gmail.com>
2021-07-25 02:18:39 +0300

    added user interactions using ModelForm

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,7 +1,11 @@
 from meetings.models import Meeting
-from django.shortcuts import render,  get_object_or_404
+from django.shortcuts import redirect, render,  get_object_or_404, redirect 
+
+# from django.forms import modelform_factory
 
 from .models import Meeting, Room
+
+from .forms import MeetingForm
 
 # Create your views here.
 
@@ -16,3 +20,20 @@ def room_list(request):
     return render(request, "meetings/rooms_list.html", 
         {"rooms": Room.objects.all()}
     )
+
+# adding a new meeting by the user
+
+# MeetingForm = modelform_factory(Meeting, exclude=[])
+
+def new(request):
+    if request.method == "POST":
+        # form has been submitted, process the data
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('welcome')
+        
+    else:
+        form = MeetingForm()
+    
+    return render(request, "meetings/new.html", {"form": form})
